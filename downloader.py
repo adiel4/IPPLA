552c0f18ba8adfba92a8d64fffc66486da9b0339
adiel4 <46599090+adiel4@users.noreply.github.com>
2023-11-01 10:49:25 +0600

    1

--- downloader.py
+++ downloader.py
@@ -1,6 +1,5 @@
 import os
 import re
-
 import aiohttp
 from PyQt6.QtCore import pyqtSlot
 from PyQt6.QtWidgets import (
@@ -8,6 +7,7 @@ from PyQt6.QtWidgets import (
     QPushButton, QTextEdit, QMessageBox
 )
 import sys
+import asyncio
 
 
 class Downloader(QWidget):
@@ -22,14 +22,14 @@ class Downloader(QWidget):
         x = (screen.width() - 800) // 2
         y = (screen.height() - 400) // 2
         self.setGeometry(x, y, 800, 400)
-        self.username_label = QLabel("Введите пользователя:")
+        self.username_label = QLabel("Enter username:")
         self.username_input = QLineEdit(self)
         self.username_input.setText('vgophap')
-        self.password_label = QLabel("Введите пароль:")
+        self.password_label = QLabel("Enter password:")
         self.password_input = QLineEdit(self)
         self.password_input.setText('adiletI1')
         self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
-        self.download_button = QPushButton("Скачать")
+        self.download_button = QPushButton("Download")
         self.download_button.clicked.connect(self.download)
         self.log_output = QTextEdit()
 
@@ -46,43 +46,44 @@ class Downloader(QWidget):
     def show_success_message():
         msg_box = QMessageBox()
         msg_box.setIcon(QMessageBox.Icon.Information)
-        msg_box.setWindowTitle("Успех")
-        msg_box.setText("Загрузка успешно завершена.")
+        msg_box.setWindowTitle("Success")
+        msg_box.setText("Download completed successfully.")
         msg_box.exec()
 
     @pyqtSlot()
     def download(self):
         username = self.username_input.text()
         password = self.password_input.text()
-        target_dir = QFileDialog.getExistingDirectory(self, "Выберите папку")
+        target_dir = QFileDialog.getExistingDirectory(self, "Choose a directory")
         if not target_dir:
-            self.log_output.append("Не выбрана папка.")
+            self.log_output.append("No directory selected.")
             return
-        file_paths = QFileDialog.getOpenFileNames(self, "Выберите текстовый файл", "", "Text files (*.txt)")
+        file_paths = QFileDialog.getOpenFileNames(self, "Choose a text file", "", "Text files (*.txt)")
         if not file_paths[0]:
-            self.log_output.append("Не выбран файл.")
+            self.log_output.append("No file selected.")
             return
 
-    @pyqtSlot()
-    def download_finished(self):
-        self.log_output.append("Download process completed.")
+        for file_path in file_paths[0]:
+            with open(file_path, 'r') as file:
+                for line in file:
+                    line = line.strip()
+                    asyncio.run(self.download_file(username, password, line, target_dir))
 
     async def download_file(self, username, password, url, target_dir):
-        async with self.session.get(url, auth=aiohttp.BasicAuth(username, password)) as response:
-            if response.status == 200:
-                # Extract the date from the URL
-                date_match = re.search(r'\d{8}', url)  # Assumes the date is an 8-digit number in the URL
-                if date_match:
-                    date = date_match.group()
-                    extension = os.path.splitext(url)[-1]
-                    filename = f"{date}{extension}"
-                    save_path = os.path.join(target_dir, filename)
-                    with open(save_path, 'wb') as f:
-                        f.write(await response.read())
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, auth=aiohttp.BasicAuth(username, password), ssl=False) as response:
+                if response.status == 200:
+                    date_match = re.search(r'\d{8}', url)
+                    if date_match:
+                        date = date_match.group()
+                        extension = os.path.splitext(url)[-1]
+                        filename = f"{date}{extension}"
+                        save_path = os.path.join(target_dir, filename)
+                        with open(save_path, 'wb') as f:
+                            f.write(await response.read())
+                            self.log_output.append(f"Downloaded: {url}")
                 else:
-                    self.log_output.append(f"Failed to extract date from the URL: {url}")
-            else:
-                self.log_output.append(f"Failed to download: {url}")
+                    self.log_output.append(f"Failed to download: {url}")
 
 
 if __name__ == '__main__':
